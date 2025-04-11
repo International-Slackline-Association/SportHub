66ba3164193d9537a3573c1a771385d441118c22
Muhammad Martinez <muhammad_martinez@yahoo.com>
2025-04-11 00:59:42 -0700

    feat: adds table component using tanstack react table

--- sport-hub/src/app/events/page.tsx
+++ sport-hub/src/app/events/page.tsx
@@ -1,11 +1,15 @@
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
+import ContestTable from "./components/ContestTable";
  
 export const metadata: Metadata = {
   title: 'SportHub - Events',
 }
 
 export default async function Page() {
-    return (
-        <h1> Events </h1>
-    )
+  return (
+    <div>
+      <h1> Events </h1>
+      <ContestTable />
+    </div>
+  );
 }
