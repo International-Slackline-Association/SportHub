5ad1dd525e33d53051fc81d214d30b68fbda5a69
darkjakeco <darkjakeco@gmail.com>
2025-05-09 15:48:22 -0600

    mocks --old

---
+++ sport-hub/src/mocks/contests_with_athletes_sample.ts
@@ -0,0 +1,252 @@
+const mockContestWithAthletes = [{
+    "contestId": "104261",
+    "discipline": "7",
+    "date": "2024-11-02",
+    "name": "9th Shenxianju International Highline challenge",
+    "prize": 1,
+    "createdAt": "2025-02-01T08:32:27.000Z",
+    "profileUrl": "http://images.isa-rankings.org/public/contest/d47abf-8.jpg",
+    "country": "cn",
+    "gender": 2,
+    "city": "Shenxianju ",
+    "category": 3,
+    "normalizedName": "9th shenxianju international highline challenge",
+    "thumbnailUrl": "http://images.isa-rankings.org/public/contest/d47abf-8_thumbnail.jpg",
+    "athletes": [
+      {
+        "athleteId": "ya-cao",
+        "name": "ya",
+        "place": "1",
+        "points": 600
+      },
+      {
+        "athleteId": "boxian-li",
+        "name": "boxian",
+        "place": "2",
+        "points": 420
+      },
+      {
+        "athleteId": "xiao-chen",
+        "name": "xiao",
+        "place": "3",
+        "points": 314
+      },
+      {
+        "athleteId": "yaqi-yuan",
+        "name": "yaqi",
+        "place": "4",
+        "points": 239
+      },
+      {
+        "athleteId": "jiaxing-zheng",
+        "name": "jiaxing",
+        "place": "5",
+        "points": 181
+      },
+      {
+        "athleteId": "yulang-li",
+        "name": "yulang",
+        "place": "6",
+        "points": 134
+      },
+      {
+        "athleteId": "yuting-zhuang",
+        "name": "yuting",
+        "place": "7",
+        "points": 94
+      },
+      {
+        "athleteId": "biyun-hao",
+        "name": "biyun",
+        "place": "8",
+        "points": 59
+      },
+      {
+        "athleteId": "yanhua-lei",
+        "name": "yanhua",
+        "place": "9",
+        "points": 28
+      },
+      {
+        "athleteId": "hanxia-xiao",
+        "name": "hanxia",
+        "place": "10",
+        "points": 1
+      }
+    ]
+  },
+  {
+    "contestId": "146383",
+    "discipline": "2",
+    "date": "2022-06-10",
+    "name": "Slack on the Beach - SlackShow",
+    "prize": 330,
+    "createdAt": "2022-10-05T14:36:14.000Z",
+    "profileUrl": "http://images.isa-rankings.org/public/contest/146383-2.jpg",
+    "country": "cz",
+    "gender": 0,
+    "city": "Windy Point",
+    "infoUrl": "https://www.facebook.com/events/3062586427341533",
+    "category": 4,
+    "normalizedName": "slack on the beach - slackshow",
+    "thumbnailUrl": "http://images.isa-rankings.org/public/contest/146383-2_thumbnail.jpg",
+    "athletes": [
+      {
+        "athleteId": "marius-kitowski",
+        "name": "marius",
+        "place": "1",
+        "points": 300
+      },
+      {
+        "athleteId": "tibor-mikulec",
+        "name": "tibor",
+        "place": "2",
+        "points": 210
+      },
+      {
+        "athleteId": "michal-p-",
+        "name": "michal",
+        "place": "3",
+        "points": 157
+      },
+      {
+        "athleteId": "lukas--cerny",
+        "name": "lukas",
+        "place": "4",
+        "points": 120
+      },
+      {
+        "athleteId": "jiri-maslo",
+        "name": "jiri",
+        "place": "5",
+        "points": 91
+      },
+      {
+        "athleteId": "ondrej-ruzicka",
+        "name": "ondrej",
+        "place": "6",
+        "points": 67
+      },
+      {
+        "athleteId": "eileen--niederlova",
+        "name": "eileen",
+        "place": "7",
+        "points": 47
+      },
+      {
+        "athleteId": "honza-prochazka",
+        "name": "honza",
+        "place": "8",
+        "points": 30
+      },
+      {
+        "athleteId": "jakub--ruzicka",
+        "name": "jakub",
+        "place": "9",
+        "points": 15
+      },
+      {
+        "athleteId": "johanna-kugler",
+        "name": "johanna",
+        "place": "10",
+        "points": 1
+      },
+      {
+        "athleteId": "jiri--cerhan",
+        "name": "jiri",
+        "place": "11",
+        "points": 1
+      }
+    ]
+  },
+  {
+    "contestId": "159405",
+    "discipline": "4",
+    "date": "2019-02-15",
+    "name": "Slack that Beach #8",
+    "prize": 1,
+    "createdAt": "2019-03-31T21:24:44.000Z",
+    "profileUrl": "http://images.isa-rankings.org/public/contest/slack-that-beach-8_2019-4.jpg",
+    "country": "cl",
+    "gender": 0,
+    "city": "Viña del Mar",
+    "category": 2,
+    "normalizedName": "slack that beach #8",
+    "thumbnailUrl": "http://images.isa-rankings.org/public/contest/slack-that-beach-8_2019-4_thumbnail.jpg",
+    "athletes": [
+      {
+        "athleteId": "agustin-palomero",
+        "name": "agustin",
+        "place": "1",
+        "points": 900
+      },
+      {
+        "athleteId": "matias-paves",
+        "name": "matias",
+        "place": "1",
+        "points": 649
+      },
+      {
+        "athleteId": "randley-freitas-borges",
+        "name": "randley",
+        "place": "2",
+        "points": 503
+      },
+      {
+        "athleteId": "alisson-ferreira",
+        "name": "alisson",
+        "place": "2",
+        "points": 398
+      },
+      {
+        "athleteId": "axel-cespedez",
+        "name": "axel",
+        "place": "3",
+        "points": 318
+      },
+      {
+        "athleteId": "cristi-n-reyes",
+        "name": "cristian",
+        "place": "3",
+        "points": 252
+      },
+      {
+        "athleteId": "gino-anoso",
+        "name": "gino",
+        "place": "4",
+        "points": 148
+      },
+      {
+        "athleteId": "daniel-leiva",
+        "name": "daniel",
+        "place": "4",
+        "points": 196
+      },
+      {
+        "athleteId": "gonzalo-sanhueza",
+        "name": "gonzalo",
+        "place": "5",
+        "points": 67
+      },
+      {
+        "athleteId": "abraham-hernandez",
+        "name": "abraham",
+        "place": "5",
+        "points": 105
+      },
+      {
+        "athleteId": "matias-sepulveda",
+        "name": "matias",
+        "place": "6",
+        "points": 32
+      },
+      {
+        "athleteId": "sebastian-moraga",
+        "name": "sebastian",
+        "place": "6",
+        "points": 1
+      }
+    ]
+  }];
+  
+export { mockContestWithAthletes }
