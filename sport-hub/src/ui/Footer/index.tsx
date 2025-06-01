f058521cb1ea7a0e73089338a14831665217dfb9
Muhammad Martinez <muhammad_martinez@yahoo.com>
2025-06-01 02:18:26 -0600

    feat: updates navigation css removes reliance on tailwind

--- sport-hub/src/ui/Footer/index.tsx
+++ sport-hub/src/ui/Footer/index.tsx
@@ -1,44 +1,49 @@
 import Link from "next/link";
 import Image from "next/image";
 import { LinkType } from "@ui/Navigation";
+import styles from "./styles.module.css"
 
 const footer_items: LinkType[] = [
-    { name: "Frequently Asked Questions", href: "/faq" },
-    { name: "ISA Members and Partners", href: "/partners" },
-    { name: "Event Registration", href: "/events" },
-    { name: "Open Source", href: "https://github.com/International-Slackline-Association" },
-    { name: "About ISA Sports", href: "/about" },
-    { name: "Contact Us", href: "/contact" },
+  { name: "Frequently Asked Questions", href: "/faq" },
+  { name: "ISA Members and Partners", href: "/partners" },
+  { name: "Event Registration", href: "/events" },
+  { name: "Open Source", href: "https://github.com/International-Slackline-Association" },
+  { name: "About ISA Sports", href: "/about" },
+  { name: "Contact Us", href: "/contact" },
 ];
 
 const Footer = () => {
-    return (
-        <footer>
-            <div className="footer-grid">
-                {footer_items.map(({ href, name }) => (
-                    <Link key={name} href={href}>
-                        {name}
-                    </Link>
-                ))}
-            </div>
-            <div className="footer-column-logos">
-                <Image 
-                    className="isa-logo"
-                    src="/static/images/ISA_logo.png"
-                    alt="logo"
-                    width={623}
-                    height={112}
-                />
-                <Image
-                    className="sport-img"
-                    src="/static/images/sport-hub-logo.png"
-                    alt="logo"
-                    width={426}
-                    height={247}
-                />
-            </div>
-        </footer>
-    );
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerGrid}>
+        {footer_items.map(({ href, name }) => (
+          <Link key={name} href={href}>
+            {name}
+          </Link>
+        ))}
+      </div>
+      <div className={styles.footerColumnLogos}>
+        <div>
+          <Image
+            alt="logo"
+            className={styles.logo}
+            height={112}
+            src="/static/images/ISA_logo.png"
+            width={623}
+          />
+        </div>
+        <div>
+          <Image
+            alt="logo"
+            className={styles.logo}
+            height={247}
+            src="/static/images/sport-hub-logo.png"
+            width={426}
+          />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
