1eefd0cb72d802f2ab74e2c748965c841d034c6b
Muhammad.Martinez <Muhammad.Martinez@target.com>
2025-09-16 22:48:40 -0500

    feat: adds modal and tabs with demos

--- sport-hub/src/app/athlete-profile/page.tsx
+++ sport-hub/src/app/athlete-profile/page.tsx
@@ -3,39 +3,39 @@
 import { useState } from 'react';
 import { mockAthleteProfile } from '@mocks/athlete_profile';
 import { ProfileCard } from '@ui/ProfileCard';
-import TabNavigation, { Tab } from '@ui/TabNavigation';
 import AthleteContestsTable from './components/AthleteContestsTable';
 import AthleteWorldRecordsTable from './components/AthleteWorldRecordsTable';
 import AthleteWorldFirstsTable from './components/AthleteWorldFirstsTable';
+import { TabGroup, TabItem } from '@ui/Tab';
 
-const tabs: Tab[] = [
-  { id: "contests", label: "Contests", path: "contests" },
-  { id: "records", label: "World Records", path: "records" },
-  { id: "firsts", label: "World Firsts", path: "firsts" },
+const tabs: TabItem[] = [
+  { id: "contests", label: "Contests" },
+  { id: "records", label: "World Records" },
+  { id: "firsts", label: "World Firsts" },
 ];
 
 export default function AthleteProfilePage() {
   // TODO: Leverage routing instead of state to make this a SSR page
   const [activeTab, setActiveTab] = useState('contests');
-  
+
   return (
     <>
       <ProfileCard profile={mockAthleteProfile} />
       <section className="p-5 sm:p-0">
-        <TabNavigation
+        <TabGroup
           activeTab={activeTab}
           onTabChange={setActiveTab}
           tabs={tabs}
+          variant="secondary"
         />
-        
         {activeTab === 'contests' && (
           <AthleteContestsTable />
         )}
-        
+
         {activeTab === 'records' && (
           <AthleteWorldRecordsTable />
         )}
-        
+
         {activeTab === 'firsts' && (
           <AthleteWorldFirstsTable />
         )}
